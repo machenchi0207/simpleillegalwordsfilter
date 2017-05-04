4470efa46f8ecafdbfe3d7b35d2b8a54c7143ca1
machenchi0207 <415401620@qq.com>
2017-05-04 18:25:04 +0800

    Simple Filter

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # simpleillegalwordsfilter
 A simple illegal words filter
+
+## useage
+
+```php
+
+$wordsAdapter = new \SIWF\Words\ArrayWordsAdapter(['simple','illegal']);
+$builder = new \SIWF\Tree\Builder($wordsAdapter);
+$storage = new \SIWF\Storage\NativeStorageAdapter($builder);
+$filter = new \SIWF\Filter\Filter($storage);
+
+$txt = 'A very simple illegal words filter';
+$hint = $filter->hint($txt);
+echo $hint;
+//this will output 'simple';
+```
---
+++ src/Tree/Builder.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Machenchi
+ * Date: 2017/5/4
+ * Time: 17:32
+ * Copyright:EastSea
+ */
+
+namespace SIWF\Tree;
+
+use SIWF\Words\DicCollection;
+use SIWF\Words\WordsAdapter;
+
+/**
+ * Class Builder
+ *
+ * @package SIWF\Tree
+ */
+class Builder
+{
+    /**
+     * @var DicCollection
+     */
+    protected $dics;
+
+    /**
+     * @var string
+     */
+    protected $charset = 'UTF8';
+
+    /**
+     * Builder constructor.
+     *
+     * @param WordsAdapter $adapter
+     */
+    public function __construct(WordsAdapter $adapter)
+    {
+        $this->dics = $adapter->getWords();
+    }
+
+    /**
+     * @return TreeNode
+     */
+    public function build()
+    {
+        $root = new TreeNode();
+        foreach ($this->dics as $dic) {
+            $node = $root;
+            $len = mb_strlen($dic,$this->charset);
+            for($i = 0;$i<$len;$i++){
+                $childs = $node->getChilds();
+                $s = mb_substr($dic,$i,1,$this->charset);
+                if(array_key_exists($s, $childs)){
+                    $node = $childs[$s];
+                    continue;
+                }
+                $temp = new TreeNode();
+                $temp->setCharacter(mb_substr($dic, $i,1,$this->charset));
+                $node = $node->addChild(mb_substr($dic,$i,1,$this->charset),$temp);
+            }
+            $node->setIsEnd(true);
+        }
+        return $root;
+    }
+}
